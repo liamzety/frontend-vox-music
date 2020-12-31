95b81639e2ceb64adf1fc4fdec24511e54ffc76f
liamzety <58632095+liamzety@users.noreply.github.com>
2020-12-31 12:44:27 +0200

    M

--- public/sw.js
+++ public/sw.js
@@ -22,7 +22,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                let res = response;
+                var res = response;
 
                 if (!res) {
                     console.log('NOT IN CACHE, FETCHED FROM NETWORK!')
